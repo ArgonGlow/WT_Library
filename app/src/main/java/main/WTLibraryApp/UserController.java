58ecf5fb389e39d7d0db310db09e7dac811c386c
SebastiaanteM <bobsebas@hotmail.nl>
2022-08-25 14:39:35 +0200

    I'm the bad guy
    Pushing to main like I just don't care.

--- app/src/main/java/main/WTLibraryApp/UserController.java
+++ app/src/main/java/main/WTLibraryApp/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(maxAge=3600)
-public class PersonController {
+public class UserController {
 	
 	@Autowired
 	private WTLibService service;
