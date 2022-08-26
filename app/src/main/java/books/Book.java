42209c68678c807b3c163460720773c1e05f9f80
SebastiaanteM <bobsebas@hotmail.nl>
2022-08-26 09:41:07 +0200

    Added methods to Book

--- app/src/main/java/books/Book.java
+++ app/src/main/java/books/Book.java
@@ -1,5 +1,26 @@
 package books;
 
+import java.sql.Blob;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String title;
+	
+	private String isbn;
+	
+	private String author;
+	
+	private Blob foto;
 
 }
