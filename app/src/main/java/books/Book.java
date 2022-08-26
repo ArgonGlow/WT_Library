5b0414c50f67afbbd1bdd472ac341c1b912ee0c8
SebastiaanteM <bobsebas@hotmail.nl>
2022-08-26 09:42:13 +0200

    Again..

--- app/src/main/java/books/Book.java
+++ app/src/main/java/books/Book.java
@@ -23,4 +23,44 @@ public class Book {
 	
 	private Blob foto;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Blob getFoto() {
+		return foto;
+	}
+
+	public void setFoto(Blob foto) {
+		this.foto = foto;
+	}
+
 }
