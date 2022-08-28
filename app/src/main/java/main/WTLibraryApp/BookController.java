6ac7f2e182bb2f643cfc2c671a89fa1eac6a1e67
djameldig <110469142+djameldig@users.noreply.github.com>
2022-08-28 19:23:48 +0100

    WTlibWeb toegevoegd
    added add and delete funcitonality to the library

--- app/src/main/java/main/WTLibraryApp/BookController.java
+++ app/src/main/java/main/WTLibraryApp/BookController.java
@@ -1,11 +1,16 @@
 package main.WTLibraryApp;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @CrossOrigin(maxAge=3600)
@@ -18,5 +23,20 @@ public class BookController {
 	public List<Books> findAll(){
 		return service.allBooks();
 	}
-
+	
+	@RequestMapping(value = "books/{id}")
+	public Optional<Books> findById(@PathVariable long id) {
+		return service.findBook(id);
+	}
+	
+	@RequestMapping(value = "books/create", method = RequestMethod.POST)
+	public void create(@RequestBody Books book) {
+		service.createBook(book);
+	}
+	
+	@RequestMapping(value = "books/delete/{id}", method = RequestMethod.DELETE)
+	public void remove(@PathVariable long id) {
+		service.deleteBook(id);
+	}
+	
 }
--- app/src/main/java/main/WTLibraryApp/WTLibBookService.java
+++ app/src/main/java/main/WTLibraryApp/WTLibBookService.java
@@ -1,9 +1,11 @@
 package main.WTLibraryApp;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class WTLibBookService {
@@ -13,5 +15,17 @@ public class WTLibBookService {
 	
 	public List<Books> allBooks(){
 		return repo.findAll();
+	}
+	
+	public Optional<Books> findBook(long id) {
+		return repo.findById(id);
+	}
+	
+	public void createBook(Books book) {
+		repo.save(book);
+	}
+	
+	public void deleteBook(long id) {
+		repo.deleteById(id);
 	}
 }
