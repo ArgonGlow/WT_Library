2fd74359c8b53853e18f0b85d50fa6bf7c07b15f
apexdk <darrylorque@icloud.com>
2022-08-30 21:13:30 +0200

    Full CRUD functionality Users

--- app/src/main/java/main/WTLibraryApp/UserController.java
+++ app/src/main/java/main/WTLibraryApp/UserController.java
@@ -27,11 +27,15 @@ public class UserController {
 	@Autowired
 	private IWTUsersRepository usersRepository;
 	
+//	Returns all users from the users table.
+	
 	@GetMapping("/users")
 	public String showUsers(Model model) {
 		model.addAttribute("users", usersRepository.findAll());
 		return "/users/users";
 	}
+	
+//	Adds a new user to the users table
 	
 	@GetMapping("/users/add-user")
 	public String addUser(Users users) {
@@ -44,6 +48,38 @@ public class UserController {
 			return "/users/add-user";
 		}
 		usersRepository.save(users);
+		return "redirect:/users";
+	}
+	
+//	Updates an user from the users table
+	
+	@GetMapping("/users/edit-user/{id}")
+	public String updateUser(@PathVariable("id") long id, Model model) {
+		Users users = usersRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id: " + id));
+		
+		model.addAttribute("users", users);
+		return "users/edit-user";
+	}
+	
+	@PostMapping("/users/edit-user/{id}")
+	public String updateUserPost(@PathVariable("id") long id, Users users, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			users.setUser_id(id);
+			return "users/edit-user";
+		}
+		
+		usersRepository.save(users);
+		return "redirect:/users";
+	}
+	
+//	Deletes an user from the table.
+	
+	@GetMapping("/users/delete-user/{id}")
+	public String deleteUser(@PathVariable("id") long id, Model model) {
+		Users users = usersRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id: " + id));
+		usersRepository.delete(users);
 		return "redirect:/users";
 	}
 	
