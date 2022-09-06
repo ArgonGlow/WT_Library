b16d3c8e3e7f50805a073015c514d1cc21f2415e
ArgonGlow <koen2702@gmail.com>
2022-09-06 16:41:54 +0200

    fix copies controller

--- app/src/main/java/main/WTLibraryApp/Book/Copy/CopiesController.java
+++ app/src/main/java/main/WTLibraryApp/Book/Copy/CopiesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @CrossOrigin(maxAge=3600)
 public class CopiesController {
@@ -37,5 +38,23 @@ public class CopiesController {
 		CopiesPK id = new CopiesPK(bookId, copyId);
 		
 		service.deleteCopy(id);
+	}
+	
+	@RequestMapping(value = "copies/assign/{bookId}/{copyId}/{userId}", method = RequestMethod.PUT)
+	public void assignCopy(@PathVariable long bookId, @PathVariable long copyId, @PathVariable long userId) {
+		
+		// create copy object by combined id
+		Optional<Copies> reservedCopy = findById(bookId, copyId);
+		
+		// abort if selected id doesn't return a copy
+		if (reservedCopy.isEmpty()) {
+			System.out.println("no copy for id: " + bookId + "." + copyId);
+			return;
+		}
+		
+		// create copy-object from list and set new userId
+		Copies loanedCopy = reservedCopy.get();
+		loanedCopy.setUserId(userId);
+		service.updateCopy(loanedCopy);
 	}
 }
