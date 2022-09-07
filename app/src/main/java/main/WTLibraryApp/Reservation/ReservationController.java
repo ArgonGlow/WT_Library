fcf3436e71e52edf6f6b974c3fcef5ba90fae706
ArgonGlow <koen2702@gmail.com>
2022-09-07 12:50:28 +0200

    add cancellation with confirmation popup

--- app/src/main/java/main/WTLibraryApp/Reservation/ReservationController.java
+++ app/src/main/java/main/WTLibraryApp/Reservation/ReservationController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- app/src/main/java/main/WTLibraryApp/User/UserController.java
+++ app/src/main/java/main/WTLibraryApp/User/UserController.java
@@ -65,7 +65,7 @@ public class UserController {
 		if (result.hasErrors()) {
 			users.setUser_id(id);
 			return "users/edit-user"; 
-		} 
+		}
 		
 		service.saveUser(users);     
 		return "redirect:/users";
