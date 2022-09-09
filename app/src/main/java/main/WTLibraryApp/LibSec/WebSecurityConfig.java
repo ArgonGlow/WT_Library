ec337fd061cc2428f9a94b1349f68f90747fc9cf
ArgonGlow <koen2702@gmail.com>
2022-09-09 16:05:41 +0200

    fix style.css user access

--- app/src/main/java/main/WTLibraryApp/LibSec/WebSecurityConfig.java
+++ app/src/main/java/main/WTLibraryApp/LibSec/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/images/*").permitAll()
+			.antMatchers("/images/*","/style.css").permitAll()
 		.and()
          	.authorizeRequests()
          	.antMatchers("/","/books").hasAnyAuthority("1","2")
