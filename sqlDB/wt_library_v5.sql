732a45cfa6a77d26319c19f5c3b3fb69b70e887a
apexdk <darrylorque@icloud.com>
2022-09-15 10:27:49 +0200

    longblob --> longtext

--- sqlDB/wt_library_v5.sql
+++ sqlDB/wt_library_v5.sql
@@ -34,7 +34,7 @@ CREATE TABLE `books` (
   `title` varchar(100) DEFAULT NULL,
   `author` varchar(255) DEFAULT NULL,
   `isbn` varchar(13) NOT NULL,
-  `cover_image` longblob DEFAULT NULL
+  `cover_image` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
