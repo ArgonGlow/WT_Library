9b13c2efa0a933c3a5263e38386b5ea9dcd47e50
djameldig <110469142+djameldig@users.noreply.github.com>
2022-09-20 11:31:24 +0100

    search on timestamp

--- src/main/java/main/WTLibraryApp/Transaction/TransactionRepository.java
+++ src/main/java/main/WTLibraryApp/Transaction/TransactionRepository.java
@@ -2,7 +2,6 @@ package main.WTLibraryApp.Transaction;
 
 import java.util.List;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,8 +13,9 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
 	//finds book by keyword
 	@Query(value = "SELECT * FROM transactions WHERE "
-			+ "book_id IN (SELECT id FROM books WHERE title LIKE %:keyword%)"
-			+ "OR user_id IN (SELECT id FROM users WHERE first_name LIKE %:keyword%)"
+			+ "timestamp LIKE %:keyword% "
+			+ "OR book_id IN (SELECT id FROM books WHERE title LIKE %:keyword%) "
+			+ "OR user_id IN (SELECT id FROM users WHERE first_name LIKE %:keyword%) "
 			+ "OR user_id IN (SELECT id FROM users WHERE last_name LIKE %:keyword%)", nativeQuery = true)
 //    @Query(value = "select * from reservations where book_id like %:keyword% or author like %:keyword%", nativeQuery = true)
     List<Transaction> findByKeyword(@Param("keyword") String keyword);
